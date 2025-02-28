c9530b93289fc6f618f066d2f8191becd1645108
lemospk <kperei300@caledonian.ac.uk>
2025-02-28 23:06:25 +0000

    starting to create the tests

---
+++ tests/test_Dec2Hex.py
@@ -0,0 +1,55 @@
+import sys, os, unittest, subprocess
+from unittest.mock import patch
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from Dec2Hex import decimal_to_hex, get_valid_integer
+
+
+class Test_function_DecimalToHex(unittest.TestCase):
+
+    def test_integer_input(self):
+        self.assertEqual(decimal_to_hex(10), "A")
+        self.assertEqual(decimal_to_hex(255), "FF")
+        self.assertEqual(decimal_to_hex(42), "2A")
+
+    def test_negative_input(self):
+        self.assertEqual(decimal_to_hex(-10), "-A")
+        self.assertEqual(decimal_to_hex(-255), "-FF")
+        self.assertEqual(decimal_to_hex(-42), "-2A")
+
+    def test_zero_input(self):
+        self.assertEqual(decimal_to_hex(0), "0")
+
+class Test_Main(unittest.TestCase):
+
+    def test_input_no_argument(self): 
+        result = subprocess.run(
+            [sys.executable, "src/Dec2Hex.py"],  
+            capture_output=True,
+            text=True
+        )
+        expected_message = "Error: No input argument provided.\nUsage: python script.py <decimal_number>"
+        self.assertTrue(
+            expected_message in result.stderr or expected_message in result.stdout,
+            f"Expected '{expected_message}' but got '{result.stderr}'"
+        )
+
+class Test_Function_GetValidIntergerFuncion(unittest.TestCase):
+    
+    @patch('builtins.input', return_value='5')  
+    def test_integer_input(self, mock_input):
+        result = get_valid_integer()
+        self.assertEqual(result, 5)  
+
+    @patch('builtins.input', return_value='-10') 
+    def test_negative_input(self, mock_input):
+        result = get_valid_integer()
+        self.assertEqual(result, -10)  
+
+    @patch('builtins.input', return_value='0')  
+    def test_zero_input(self, mock_input):
+        result = get_valid_integer()
+        self.assertEqual(result, 0)  
+
+if __name__ == '__main__':
+    unittest.main()
+
